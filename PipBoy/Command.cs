392791a6a20e90d239c4ceaad70f516d06bd26b5
Philipp Weber <weberph@web.de>
2015-12-26 01:25:40 +0100

    add CommandSender

---
+++ PipBoy/Command.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace PipBoy
+{
+    public enum CommandType
+    {
+        ToggleQuest = 5,
+        ToggleRadio = 12
+    }
+
+    [DataContract]
+    public class Command
+    {
+        [DataMember(Name = "type")]
+        public CommandType CommandType { get; private set; }
+
+        [DataMember(Name = "args")]
+        public object[] Arguments { get; set; }
+
+        [DataMember(Name = "id")]
+        private uint _sequenceId;
+
+        public Command(CommandType commandType, params object[] arguments)
+        {
+            CommandType = commandType;
+            Arguments = arguments;
+        }
+
+        public string Format(uint sequenceId)
+        {
+            _sequenceId = sequenceId;
+
+            var serializer = new DataContractJsonSerializer(GetType(), new[] { typeof(int[]), typeof(uint[]), typeof(string[]) });
+            using (var ms = new MemoryStream())
+            {
+                serializer.WriteObject(ms, this);
+                return Encoding.ASCII.GetString(ms.ToArray());
+            }
+        }
+    }
+}
---
+++ PipBoy/CommandSender.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+
+namespace PipBoy
+{
+    public class CommandSender
+    {
+        private readonly Stream _stream;
+
+        private uint _sequenceId;
+
+        public CommandSender(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public void Send(Command command)
+        {
+            var sequenceId = _sequenceId++;
+            var commandString = command.Format(sequenceId);
+            var commandData = Encoding.ASCII.GetBytes(commandString);
+
+            using (var ms = new MemoryStream())
+            {
+                var bw = new BinaryWriter(ms);
+                bw.Write(commandData.Length);
+                bw.Write((byte)0x05);
+                bw.Write(commandData);
+
+                var buffer = ms.ToArray();
+                _stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
